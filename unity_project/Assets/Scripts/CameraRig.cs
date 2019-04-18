86932d457ebbfb9c282ec25a2fb91fe12f9d6adf
VTonelli4 <44092339+VTonelli4@users.noreply.github.com>
2019-04-18 13:11:47 +0200

    Skybox Sphere
    > Created a skybox sphere in blender, which uses an equirectangular UV mapping with normals inward;
    > Imported the sphere in Unity's scene;
    > Added mouse rotation support for the main camera (debug).

---
+++ unity_project/Assets/Scripts/CameraRig.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRig : MonoBehaviour
+{
+    void Update()
+    {
+        RotateMouse();
+    }
+
+    /*void RotateMouse():
+     * Rotates the camera around its current position using mouse controls. */
+    void RotateMouse()
+    {
+        //Get x and y inputs from Unity
+        float y = Input.GetAxis("Mouse X");
+        float x = Input.GetAxis("Mouse Y");
+        
+        //Create rotation vector
+        Vector3 rotateValue = new Vector3(x, y * -1, 0);
+        //Apply rotation vector
+        transform.eulerAngles = transform.eulerAngles - rotateValue;
+    }
+}
