35dd97f26f3d4f20f7d402625b4f7eb750f67419
VTonelli4 <44092339+VTonelli4@users.noreply.github.com>
2020-01-03 13:17:22 +0100

    Merge Fix

--- unity_project/Assets/Scripts/Singletons/Master.cs
+++ unity_project/Assets/Scripts/Singletons/Master.cs
@@ -53,16 +53,13 @@ public class Master : MonoBehaviour {
         state_machine.Init(this, sphere_text);
         main_camera.Init();
 
-        //Init Texts for sphere
         sphere_text.Init(GameObject.Find("Main Camera").GetComponent<Camera>());
-        // Add cardinal points
         sphere_text.AddText("North Text", "North", new Vector2(0, 0), north_sphere);
         sphere_text.AddText("South Text", "South", new Vector2(180, 0), north_sphere);
         sphere_text.AddText("East Text", "East", new Vector2(90, 0), north_sphere);
         sphere_text.AddText("West Text", "West", new Vector2(270, 0), north_sphere);
-        sphere_text.AddText("Bottom Text", "Zenith", new Vector2(0, -90), north_sphere);
-        sphere_text.AddText("Top Text", "Nadir", new Vector2(0, 90), north_sphere);
-
+        sphere_text.AddText("Bottom Text", "Nadir", new Vector2(0, -90), north_sphere);
+        sphere_text.AddText("Top Text", "Zenith", new Vector2(0, 90), north_sphere);
         //Load all event summaries.
         GW_event_db.LoadEventSummaries();
 
